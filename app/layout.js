adc15e87b9cf3b05e2b887e7ee99ef935ba9c236
Yashpreet Singh <yashpreet1686@gmail.com>
2025-07-19 19:28:52 +0530

    add next auth and modify sidebar component

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
 import News from "@/components/News";
+import SessionWrapper from "@/components/SessionWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,23 +22,29 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <div className="flex justify-between max-w-7xl mx-auto">
-          <div className="hidden sm:inline border-r h-screen">
-            <Sidebar />
-          </div>
-          <div>{children}</div>
-          <div className="lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]">
-            <div className="sticky top-0 bg-white py-2">
-              <input type="text" placeholder="Search..." className="bg-gray-100 border border-gray-200 rounded-3xl text-sm w-full px-4 py-2" />
+    <SessionWrapper>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <div className="flex justify-between max-w-7xl mx-auto">
+            <div className="hidden sm:inline border-r h-screen">
+              <Sidebar />
             </div>
-            <News />
+            <div>{children}</div>
+            <div className="lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]">
+              <div className="sticky top-0 bg-white py-2">
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  className="bg-gray-100 border border-gray-200 rounded-3xl text-sm w-full px-4 py-2"
+                />
+              </div>
+              <News />
+            </div>
           </div>
-        </div>
-      </body>
-    </html>
+        </body>
+      </html>
+    </SessionWrapper>
   );
 }
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,9 +1,13 @@
+'use client';
 import Link from 'next/link';
 import React from 'react'
 import { FaXTwitter } from "react-icons/fa6";
 import { HiHome } from "react-icons/hi";
+import { signIn, signOut, useSession } from "next-auth/react"
 
 const Sidebar = () => {
+
+  const { data: session } = useSession();
   return (
     <div className='flex flex-col gap-4 p-3'>
       <Link href='/'>
@@ -13,8 +17,20 @@ const Sidebar = () => {
         <HiHome className='h-7 w-7' />
         <span className='font-bold hidden xl:inline'>Home</span>
       </Link>
-      <button className='bg-blue-400 text-white font-bold rounded-full mt-4 hover:brightness-95 hover:bg-blue-600 transition-all duration-200 w-48 h-9 shadow-md cursor-pointer hidden xl:inline'>Sign In</button>
-    </div>
+      {
+        session
+          ? (
+            <button className='bg-blue-400 text-white font-bold rounded-full mt-4 hover:brightness-95 hover:bg-blue-600 transition-all duration-200 w-48 h-9 shadow-md cursor-pointer hidden xl:inline' onClick={() => signOut()}>
+              Sign Out
+            </button>
+          )
+          : (
+            <button className='bg-blue-400 text-white font-bold rounded-full mt-4 hover:brightness-95 hover:bg-blue-600 transition-all duration-200 w-48 h-9 shadow-md cursor-pointer hidden xl:inline' onClick={() => signIn()}>
+              Sign In
+            </button>
+          )
+      }
+    </div >
   )
 }
 
