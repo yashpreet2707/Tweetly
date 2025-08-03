aa66f76b77a907fd5e72655b99598719f3edab08
Yashpreet Singh <yashpreet1686@gmail.com>
2025-08-03 22:21:43 +0530

    fix minor bug

--- components/ThemeToggler.jsx
+++ components/ThemeToggler.jsx
@@ -7,10 +7,10 @@ const ThemeToggler = () => {
     const { theme, setTheme } = useTheme();
     return (
         <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')} className='p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700 transition cursor-pointer' aria-label='Toggle Theme'>
-            {theme === 'dark' ? (
-                <HiSun className='text-white h-6 w-6' />
-            ) : (
+            {theme === 'light' ? (
                 <HiMoon className='h-6 w-6' />
+            ) : (
+                <HiSun className='text-white h-6 w-6' />
             )}
         </button>
     )
